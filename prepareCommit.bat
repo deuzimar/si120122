5a0dba2752f5703e2e5870b7132539bf2925f77e
deuzimar <deuzimarsv@hotmail.com>
2013-11-14 00:40:18 -0200

    mtos testes

--- prepareCommit.bat
+++ prepareCommit.bat
@@ -1,7 +1,6 @@
-git add .
-git rm --cached **/*.project
+git add . lalalalala
+git rm --cached **/*.project lololol
 git rm -r --cached  **/.settings
 git rm -r --cached  **/.classpath
-
 git rm -r --cached  **/build
 git rm -r --cached  **/bin
